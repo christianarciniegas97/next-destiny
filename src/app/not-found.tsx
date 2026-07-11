a2c33cc54d2bef8007b7395cfd0414871406de71
chris barrios lider tecnico <christianeadres97@gmail.com>
2026-07-11 18:28:05 -0300

    FIXTURE/DN-03

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center">    
-      <Image className="w-full h-[500px] object-cover"
+    <div className="p-2 flex flex-col items-center">    
+      <Image className="w-full lg:h-[500px] object-cover  md:h-0"
         src={imagen404}
         alt="pagina de error"
       >
    
     </Image>
-      <Link href="/"  className="btn btn-primary mt-6 btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl" >Return Home</Link>
+      <Link href="/"  className="btn btn-primary mt-6 btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl" >Vuelve a pagina de inicio</Link>
     </div>
   )
 }
